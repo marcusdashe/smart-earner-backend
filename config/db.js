6cbdb9cf3340aa71cc5591c97162f21078a4f33c
Marcus Dashe <marcusdashe.developer@gmail.com>
2022-07-28 17:21:11 +0100

    Fix MongoDB Connection Error...Deployment

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-// const MOGOURL = process.env.MONGO_URL || `mongodb://localhost:27017/smartEarners`
-const MOGOURL = `mongodb://localhost:27017/smartEarners`
+const MOGOURL = process.env.MONGO_URL
+// const MOGOURL = `mongodb://localhost:27017/smartEarners`
 
 module.exports = async() =>{
     try{
